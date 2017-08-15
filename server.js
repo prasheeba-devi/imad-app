796c4da2b3f6a40dd95ee3c75b938a0ba317c39f
prasheeba-devi <30875104+prasheeba-devi@users.noreply.github.com>
2017-08-15 13:45:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/articleone', function (req, res) {
-  res.send('Article One requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/articletwo', function (req, res) {
   res.send('Article Two requested and will be served here');
