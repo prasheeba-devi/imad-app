429e0333464dd10371d38bc92db6afb52fbc9c35
prasheeba-devi <30875104+prasheeba-devi@users.noreply.github.com>
2017-08-17 18:05:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,29 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // make a request to the server and send the name
+    // create a request object
+    var request = new XMLHttpRequest();
     
+    // capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+            // DO SOME ACTION
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+
+    //Make the request
+    request.open('GET','http://prasheebaganeshan.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);    
     //capture a list of names and render it as a list
     
     var names = ['name1', 'name2','name3','name4'];
